07c063e534b59680dc26f4f67b9920e3b2513624
lucatescari <luca@ltescari.ch>
2019-03-29 10:16:00 +0100

    made ready for editions

--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,43 @@ const cssFramework = 'bs4';
 const appname = 'badAppNamePlsRename';
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
-module.exports = {
-  entry: ['./src/scss/' + cssFramework + '.scss', './src/ts/main.ts'],
-  watch: true,
-  mode: 'none',
-  output: {
-    filename: 'app-bundle.min.js',
-    library: appname,
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss']
-  },
-  module: {
-    rules: [{
-        test: /\.scss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-          }
-        ]
-      },
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.min.css'
-    })
-  ]
+module.exports = env => {
+  
+  return {
+    entry: ['./src/scss/' + cssFramework + '.scss', './src/ts/main.ts'],
+    watch: true,
+    mode: 'none',
+    output: {
+      path: path.resolve(__dirname, ((env && env.staging) ? 'staging/dist' : 'live/dist')),
+      filename: 'app-bundle.min.js',
+      library: appname,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.scss']
+    },
+    module: {
+      rules: [{
+          test: /\.scss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+            }
+          ]
+        },
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.min.css'
+      })
+    ]
+  };
 };
